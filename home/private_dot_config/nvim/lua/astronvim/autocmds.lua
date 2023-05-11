b89821fa0b65cd23b21736735ca6119857805e1d
abr <aloys.berger@technichegroup.com>
2023-05-11 15:07:55 +1000

    Improve nvim autocmd

--- home/private_dot_config/nvim/lua/astronvim/autocmds.lua
+++ home/private_dot_config/nvim/lua/astronvim/autocmds.lua
@@ -12,7 +12,7 @@ local astroevent = utils.event
 vim.cmd [[
 augroup kitty_mp
     autocmd!
-    au VimLeave * :silent !kitty @ set-spacing padding=20 margin=10
+    au VimLeave * :silent !kitty @ set-spacing padding=5 margin=2
     au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0
 augroup END
 ]]

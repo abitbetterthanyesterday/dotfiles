5c6e2f47cfd3bb3ea1bda10da352ca7c34ddc7c6
abr <aloys.berger@technichegroup.com>
2023-05-12 14:35:47 +1000

    Reorganise nvim's lua files

--- home/private_dot_config/nvim/lua/user/init.lua
+++ home/private_dot_config/nvim/lua/user/init.lua
@@ -1,12 +1,5 @@
-local config = {
-  -- When using kitty, set the padding/margin
-  vim.cmd [[
-  augroup kitty_mp
-      autocmd!
-      au VimLeave * :silent !kitty @ set-spacing padding=5 margin=2
-      au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0
-  augroup END
-  ]]
-}
+require('user.autocmds')
+
+local config = {}
 
 return config
